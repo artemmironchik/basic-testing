1e807fc0eb4078b8d62ffb8cccc9448d9c9237a6
Artem Mironchik <tawerka.power@gmail.com>
2024-02-11 21:14:57 +0300

    feat: implement partial mocking tests

--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -1,20 +1,45 @@
 // Uncomment the code below and write your tests
-// import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
+import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
 
 jest.mock('./index', () => {
-  // const originalModule = jest.requireActual<typeof import('./index')>('./index');
+  const originalModule =
+    jest.requireActual<typeof import('./index')>('./index');
+
+  return {
+    __esModule: true,
+    ...originalModule,
+    mockOne: jest.fn(() => undefined),
+    mockTwo: jest.fn(() => undefined),
+    mockThree: jest.fn(() => undefined),
+  };
 });
 
 describe('partial mocking', () => {
+  let spy;
+
+  beforeAll(() => {
+    spy = jest.fn();
+  });
+
   afterAll(() => {
     jest.unmock('./index');
   });
 
   test('mockOne, mockTwo, mockThree should not log into console', () => {
-    // Write your test here
+    spy = jest.spyOn(console, 'log');
+
+    mockOne();
+    mockTwo();
+    mockThree();
+
+    expect(spy).not.toHaveBeenCalled();
   });
 
   test('unmockedFunction should log into console', () => {
-    // Write your test here
+    spy = jest.spyOn(console, 'log');
+
+    unmockedFunction();
+
+    expect(spy).toHaveBeenCalled();
   });
 });
